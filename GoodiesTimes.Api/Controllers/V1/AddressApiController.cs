d2cdbfad9000721ba2554e8f898f7c64b1cf830a
Rodrigo Castro Moura <rorigo.castro.moura@gmail.com>
2018-01-08 00:54:43 -0200

    Com contreler address

---
+++ GoodiesTimes.Api/Controllers/V1/AddressApiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AutoMapper;
+using GoodiesTime.Domain.DTO;
+using GoodiesTime.Library;
+using GoodiesTime.Domain.Interfaces.Services;
+using GoodiesTimes.Api.Models;
+using System.Web.Http.Description;
+
+namespace GoodiesTimes.Api.Controllers.V1
+{
+    public class AddressApiController : ApiController
+    {
+        private readonly Itb_partnersServices service;
+
+        public AddressApiController(Itb_partnersServices service)
+        {
+            this.service = service;
+        }
+
+
+    }
+}

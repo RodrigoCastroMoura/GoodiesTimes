5fca3ddc65262dc2bfa0b011d0c6c1e79c11ecb4
Rodrigo <rodrigo_castro_moura@hotmail.com>
2018-01-22 14:40:07 -0200

    Projeto Add MVC

---
+++ GoodiesTimes.Api/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoodiesTimes.Api.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- GoodiesTimes.Api/Global.asax.cs
+++ GoodiesTimes.Api/Global.asax.cs
@@ -1,14 +1,7 @@
 ﻿using GoodiesTimes.Api.Mappers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 using System.Web.Http;
-using System.Web.Http.Dependencies;
+
 
 namespace GoodiesTimes.Api
 {
@@ -21,6 +14,7 @@ namespace GoodiesTimes.Api
 
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AutoMapperConfig.RegisterMappings();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
 
         }
 
